f7f4bc31f3bb2cc115f6339ae6799e4d45b51148
Matthew Henderson <info@matthewahenderson.com>
2021-02-21 18:27:17 -0500

    Added black shading function, option to choose color, and comments to app.js; added formatting for buttons CSS

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
-//Create 16x16 grid of divs
+// Create grid to store divs
 const container = document.getElementById('container');
 
 function createDivs() {
+    // Define gride size
     let gridSize = prompt("How large do you want your grid?");
+    // Select rainbow or black squares
+    let colorSelection = prompt("Please enter rainbow or black");
     let gridSizeNum = parseInt(gridSize);
-    if (gridSizeNum > 100 || gridSizeNum < 4 || isNaN(gridSizeNum) == true) {
-        alert("Please enter a valid number between 4 and 100");
+    // Check to make sure grid size is accetable
+    if (gridSizeNum > 25 || gridSizeNum < 4 || isNaN(gridSizeNum) == true) {
+        alert("Please enter a valid number between 4 and 25");
         createDivs();
     } else {
+        // Create cells with either ranbow or black colors
         const cellDimension = 500/gridSizeNum;
         let root = document.documentElement;
         root.style.setProperty('--height', cellDimension + 'px');
         root.style.setProperty('--width', cellDimension + 'px');
         for (let i = 0; i < (gridSizeNum*gridSizeNum); i++) {    
-            const row = document.createElement('div');
-            row.classList = 'row';
-            container.appendChild(row);
+            const cell = document.createElement('div');
+            cell.classList = 'cell';
+            container.appendChild(cell);
+            if (colorSelection == 'rainbow')
+                cell.addEventListener("mouseover", rainbowColors);
+            else {
+                cell.addEventListener("mouseover", blackShading);
+            }
         }
     }
 }
 
+// Creates array of child nodes in container and removes divs so new grid can be created
 function deleteDivs() {
     let grid = Array.from(container.childNodes);
     grid.forEach((element) => {
         container.removeChild(element);           
     })
 }
+
+// Generates a random color
+function rainbowColors(e) {
+    let colorA = Math.floor(Math.random() * 256);
+    let colorB = Math.floor(Math.random() * 256);
+    let colorC = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${colorA}, ${colorB}, ${colorC})`;
+}
+
+// Shades the cell black on mouseover
+function blackShading(e) {
+    if (e.target.style.backgroundColor = 'white') {
+        e.currentTarget.style.backgroundColor = 'black';
+    }
+} 
